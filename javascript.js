79a3e4e9395829cd3f6e945457e8f14f1ca105e4
srinivasareddy4040 <srinivasreddykandi45@gmail.com>
2025-04-30 19:45:20 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.getElementById("helpme").addEventListener("click", function() {
+    alert("Thank you for clicking! For further help, please contact support or visit our FAQ page.");
+});
+
+
+
